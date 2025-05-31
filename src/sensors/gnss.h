dfd30db9e65e10384c4a712e1c41901a88b2ceda
Simen Eilevstjønn <simene@lyse.net>
2025-05-31 21:11:44 +0200

    GNSS

---
+++ src/sensors/gnss.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <array>
+#include <cstdint>
+#include <cstddef>
+#include <queue>
+#include <string>
+
+class GNSS
+{
+  public:
+    GNSS();
+
+    void tick();
+
+    std::queue<std::string>& get_fifo();
+
+  private:
+    int m_file;
+    std::array<uint8_t, 200> m_packet;
+    size_t m_packet_index = 0;
+
+    std::queue<std::string> m_fifo;
+
+    void decode_bytes(const uint8_t* data, size_t len);
+    void process_packet(const uint8_t* data, size_t len);
+};
